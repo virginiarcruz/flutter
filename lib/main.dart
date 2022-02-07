85678226936706e38e96b0005c4d76324a3c7e60
Virginia Cruz <virginia.cruz@nubank.com.br>
2022-02-07 18:57:16 -0300

    feat: initial commit with the first component

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      Column(
+        children: <Widget>[
+          Text(
+            'Deliver features faster',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Craft beautiful UIs',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Craft beautiful UIsqqqqq',
+            textDirection: TextDirection.ltr,
+          ),
+          Column(children: <Widget>[
+            Text(
+              'Craft beautiful UIs',
+              textDirection: TextDirection.ltr,
+            ),
+          ])
+        ],
+      ),
+    );
