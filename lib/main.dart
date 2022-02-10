06e0b73e654b6665d6ce63d3f3cdd4084a3bb3b7
Virginia Cruz <virginia.cruz@nubank.com.br>
2022-02-10 18:10:44 -0300

    fund de flutter: 2 implementando layouts com widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,50 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      Column(
-        children: <Widget>[
-          Text(
-            'Deliver features faster',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            'Craft beautiful UIs',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            'Craft beautiful UIsqqqqq',
-            textDirection: TextDirection.ltr,
-          ),
-          Column(children: <Widget>[
-            Text(
-              'Craft beautiful UIs',
-              textDirection: TextDirection.ltr,
-            ),
-          ])
-        ],
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        body: ListaTransferencias(),
+        appBar: AppBar(
+          title: Text('Transferências'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
       ),
+    ));
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ItemTransferencias(Transferencia(100.0, 1000)),
+        ItemTransferencias(Transferencia(200.0, 2000)),
+        ItemTransferencias(Transferencia(300.0, 3000)),
+      ],
     );
+  }
+}
+
+class ItemTransferencias extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencias(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+      leading: Icon(Icons.monetization_on),
+      title: Text(_transferencia.valor.toString()),
+      subtitle: Text(_transferencia.numeroConta.toString()),
+    ));
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
