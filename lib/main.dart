8eabace53a1afd263a6c69de266dd7969ab516de
Virginia Cruz <virginia.cruz@nubank.com.br>
2022-02-15 20:15:26 -0300

    persistencia: aula 1 - dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,62 @@ class BytebankApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primaryColor: Colors.green[900],
-        accentColor: Colors.blue[700],
-        buttonTheme: ButtonThemeData(
-          textTheme: ButtonTextTheme.primary,
-        )
+          primaryColor: Colors.green[900],
+          accentColor: Colors.blueAccent[700],
+          buttonTheme: ButtonThemeData(
+            buttonColor: Colors.blueAccent[700],
+            textTheme: ButtonTextTheme.primary,
+          )
       ),
-      home: ListaTransferencias(),
+      home: Dashboard(),
+      // theme: ThemeData(
+      //   primaryColor: Colors.green[900],
+      //   accentColor: Colors.blue[700],
+      //   buttonTheme: ButtonThemeData(
+      //     textTheme: ButtonTextTheme.primary,
+      //   )
+      // ),
+      // home: ListaTransferencias(),
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Dashboard'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('images/bytebank_logo.png'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                padding: EdgeInsets.all(8.0),
+                height: 100,
+                width: 150,
+                color: Theme.of(context).primaryColor,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Icon(Icons.people, color: Colors.white, size: 24.0,),
+                    Text('Contacts', style: TextStyle(color: Colors.white, fontSize: 16.0,))
+                  ],
+                ),
+              ),
+            )
+          ],
+        )
     );
   }
 }
