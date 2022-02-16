550e38537fb8ff9532753707c5b6b7e8d78b78de
Virginia Cruz <virginia.cruz@nubank.com.br>
2022-02-16 11:39:29 -0300

    persistencia: aula 2 - fluxo do app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/dashboard.dart';
 import 'screens/transferencia/lista.dart';
 
 void main() => runApp(BytebankApp());
@@ -30,42 +31,4 @@ class BytebankApp extends StatelessWidget {
   }
 }
 
-class Dashboard extends StatelessWidget {
-  const Dashboard({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Dashboard'),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset('images/bytebank_logo.png'),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                padding: EdgeInsets.all(8.0),
-                height: 100,
-                width: 150,
-                color: Theme.of(context).primaryColor,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Icon(Icons.people, color: Colors.white, size: 24.0,),
-                    Text('Contacts', style: TextStyle(color: Colors.white, fontSize: 16.0,))
-                  ],
-                ),
-              ),
-            )
-          ],
-        )
-    );
-  }
-}
---
+++ lib/screens/dashboard.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'contacts_list.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Dashboard'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('images/bytebank_logo.png'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Material(
+                color: Theme.of(context).primaryColor,
+                child: InkWell(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      (MaterialPageRoute(
+                        builder: (context) => ContactsList(),
+                      )),
+                    );
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(8.0),
+                    height: 100,
+                    width: 150,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Icon(
+                          Icons.people,
+                          color: Colors.white,
+                          size: 24.0,
+                        ),
+                        Text('Contacts',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16.0,
+                            ))
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ));
+  }
+}
