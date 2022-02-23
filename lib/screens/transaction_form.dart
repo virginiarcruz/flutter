cfc59095108fa2728e73397d75dd95518d978e0f
Virginia Cruz <virginia.cruz@nubank.com.br>
2022-02-23 17:35:22 -0300

    curso 4 comunic http: aula 5

--- lib/screens/transaction_form.dart
+++ lib/screens/transaction_form.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:bytebank/components/progress.dart';
 import 'package:bytebank/components/response_dialog.dart';
 import 'package:bytebank/http/transaction_auth_dialog.dart';
 import 'package:bytebank/http/webclients/transaction_webclient.dart';
@@ -22,6 +23,8 @@ class _TransactionFormState extends State<TransactionForm> {
   final TransactionWebClient _webClient = TransactionWebClient();
   final String transactionId = Uuid().v4();
 
+  bool _sending = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,6 +37,15 @@ class _TransactionFormState extends State<TransactionForm> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
+              Visibility(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Progress(
+                    message: 'Sending..',
+                  ),
+                ),
+                visible: _sending,
+              ),
               Text(
                 widget.contact.name,
                 style: TextStyle(
@@ -100,6 +112,7 @@ class _TransactionFormState extends State<TransactionForm> {
       password,
       context,
     );
+
     _showSuccessfulMessage(transaction, context);
   }
 
@@ -117,6 +130,9 @@ class _TransactionFormState extends State<TransactionForm> {
 
   Future<Transaction> _send(Transaction transactionCreated, String password,
       BuildContext context) async {
+    setState(() {
+      _sending = true;
+    });
     final Transaction transaction =
         await _webClient.save(transactionCreated, password).catchError((e) {
       _showFailureMessage(context, message: e.message);
@@ -125,7 +141,12 @@ class _TransactionFormState extends State<TransactionForm> {
     }, test: (e) => e is HttpException).catchError((e) {
       _showFailureMessage(context,
           message: 'timeout submitting the transaction');
+    }).whenComplete(() {
+      setState(() {
+        _sending = false;
+      });
     });
+
     return transaction;
   }
 
